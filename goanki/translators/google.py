fb0d8aa4e41f8c8bb34183b65553aa6a55421597
Guanya Peng <guanya.peng24@gmail.com>
2024-10-21 15:09:49 +0200

    Fix type hinting issues

--- goanki/translators/google.py
+++ goanki/translators/google.py
@@ -6,3 +6,11 @@ from typing import Optional
 
 from bs4 import BeautifulSoup
 
+from .base import HTTPTranslator, TranslationResult, ensure_language
+from .registry import register
+
+
+@register
+class GoogleTranslator(HTTPTranslator):
+    """Fetch translations from the public Google Translate mobile endpoint."""
+
