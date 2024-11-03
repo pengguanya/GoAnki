29ebf1c389c4b95fc76702b3b8a4342add181125
Guanya Peng <guanya.peng24@gmail.com>
2024-11-03 23:44:10 +0100

    Optimize performance

--- goanki/service.py
+++ goanki/service.py
@@ -4,3 +4,9 @@ from __future__ import annotations
 
 import json
 import logging
+import os
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, Iterable, List, Sequence
+
