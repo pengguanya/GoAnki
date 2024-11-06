bf368375e13d06da3942edddbfa8d26ff13c7ad6
Guanya Peng <guanya.peng24@gmail.com>
2024-11-06 22:02:52 +0100

    Flesh out implementation of registry.py

--- goanki/translators/registry.py
+++ goanki/translators/registry.py
@@ -4,3 +4,9 @@ from __future__ import annotations
 
 from typing import Dict, Iterable, Type
 
+from .base import Translator
+
+
+class TranslatorRegistry:
+    """Registry of available translator classes."""
+
