d9b8c014f8e12d1a8f745a1bafdc79473f54e694
Guanya Peng <guanya.peng24@gmail.com>
2024-11-17 20:39:57 +0100

    Implement design patterns

--- google_translator.py
+++ google_translator.py
@@ -6,3 +6,11 @@ import re
 import requests
 import threading
 
+class TransCrawler:
+    '''Web Crawler to get the translated word from google translation'''
+    def __init__(self, inlang, outlang, theword):
+        self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        self.headers = {'User-Agent' : self.user_agent}
+        self.inlang = inlang
+        self.outlang = outlang
+        self.theword = theword
