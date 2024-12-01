4285fd9d5c34f64520464d89aaeac13c8522f195
Guanya Peng <guanya.peng24@gmail.com>
2024-12-01 20:21:23 +0100

    Refactor class inheritance

--- goanki/io/reader.py
+++ goanki/io/reader.py
@@ -4,3 +4,9 @@ from __future__ import annotations
 
 import re
 from pathlib import Path
+from typing import Iterable, List
+
+from goanki.translators.base import deduplicate_preserve_order
+
+DATE_REGEX = re.compile(r"\d{2}\.\d{2}\.\d{4}")
+
