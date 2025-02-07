542fda48435e992f860bad29fa4435c71c0e18f0
Guanya Peng <guanya.peng24@gmail.com>
2025-02-07 14:15:50 +0100

    Add unit tests

--- goanki/translators/linguee.py
+++ goanki/translators/linguee.py
@@ -4,3 +4,9 @@ from __future__ import annotations
 
 from typing import Optional
 from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+
+from .base import HTTPTranslator, TranslationResult, ensure_language
+from .registry import register
+
