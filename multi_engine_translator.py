4720a15da575f45adbdfa40a79d25711d5c2531d
Guanya Peng <guanya.peng24@gmail.com>
2025-03-07 22:03:08 +0100

    Add dependency usage in multi_engine_translator.py

--- multi_engine_translator.py
+++ multi_engine_translator.py
@@ -10,3 +10,9 @@ from bs4 import BeautifulSoup
 from collections import deque
 
 class fetch_page:
+    user_agent = 'mozilla/4.0 (compatible; msie 5.5; windows nt)'
+    headers = {'user-agent' : user_agent}
+
+    @classmethod
+    def getpage(cls, url, keywords):
+        try:
