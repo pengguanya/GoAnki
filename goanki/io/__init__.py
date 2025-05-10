09dae65e7bf97d9590b554295599c9b1553f2a71
Guanya Peng <guanya.peng24@gmail.com>
2025-05-10 17:44:34 +0200

    Create __init__.py

---
+++ goanki/io/__init__.py
@@ -0,0 +1,10 @@
+"""I/O utilities for GoAnki."""
+
+from __future__ import annotations
+
+from .config import AppConfig, TargetSpec, config_from_mapping, load_config, override_config, parse_target_option
+from .reader import read_input_words
+from .writer import write_records
+
+__all__ = [
+    "AppConfig",
