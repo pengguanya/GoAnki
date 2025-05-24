6869e6677a251d252aedcf9f9bb19d748970308c
Guanya Peng <guanya.peng24@gmail.com>
2025-05-24 12:32:38 +0200

    Add edge case handling to multi_engine_translator.py

--- multi_engine_translator.py
+++ multi_engine_translator.py
@@ -138,3 +138,73 @@ class linguee:
 
 class TransCrawler:
     '''Web Crawler to get the translated word from google translation'''
+    def __init__(self, inlang, outlang, theword):
+        self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        self.headers = {'User-Agent' : self.user_agent}
+        self.inlang = inlang
+        self.outlang = outlang
+        self.theword = theword
+        self.keywords ={'sl': self.inlang, 'tl': self.outlang, 'ie':'UTF-8', 'q': self.theword}
+        self.url = 'http://translate.google.com/m'
+
+    def getPage(self):
+        url = self.url
+        keywords = self.keywords
+        try:
+            response = requests.get(url, keywords, headers = self.headers)
+            content = response.text
+            return content
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return None
+
+    def getWord(self):
+        mark='class="t0">'
+        content = self.getPage()
+        if not content:
+            print('Load page failed!')
+            return None
+        else:
+            #return content
+            startpos = content.index(mark)
+            remaincont = content[content.find(mark)+len(mark):]
+            result = remaincont.split('<')[0]
+            return result
+
+# This function is useful only when multithread is switch off
+# Because multithread will destroy the order later
+# If set() is slower than O(n), this can be used for the speed purpose
+# def del_dups(seq):
+#     '''function to delete duplicate while preserve order'''
+#     seen = {}
+#     newlist = []
+#     for item in seq:
+#         if item not in seen:
+#             seen[item] = True
+#             newlist.append(item)
+#     return newlist
+def html_decode(s):
+    """
+    Returns the ASCII decoded version of the given HTML string. This does
+    NOT remove normal HTML tags like <p>.
+    """
+    htmlCodes = (
+            ("'", '&#39;'),
+            ('"', '&quot;'),
+            ('>', '&gt;'),
+            ('<', '&lt;'),
+            ('&', '&amp;')
+        )
+    for code in htmlCodes:
+        s = s.replace(code[1], code[0])
+    return s
+
+def is_sentance(instr):
+    if " " in instr.strip():
+        return True
+    else:
+        return False
+
+def transword_writeoutput(inword, inlang, outlang_list, outfilename):
+    '''read a word string and save the input word and the output translation into a csv file'''
+    output_list = []
