4882ed5cfba74a09f53cfd79e9d19ea488c8d302
Guanya Peng <guanya.peng24@gmail.com>
2025-08-03 10:03:59 +0200

    Add imports to multi_engine_translator.py

--- multi_engine_translator.py
+++ multi_engine_translator.py
@@ -136,3 +136,9 @@ class linguee:
             inwordfull = inword
         return inwordfull
 
+class TransCrawler:
+    '''Web Crawler to get the translated word from google translation'''
+    def __init__(self, inlang, outlang, theword):
+        self.user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+        self.headers = {'User-Agent' : self.user_agent}
+        self.inlang = inlang
