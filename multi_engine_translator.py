a88ed340ad75c02383cbbac8a163c4985cb35d53
Guanya Peng <guanya.peng24@gmail.com>
2025-10-25 23:25:15 +0200

    Define type hints in multi_engine_translator.py

--- multi_engine_translator.py
+++ multi_engine_translator.py
@@ -232,3 +232,9 @@ def transword_writeoutput(inword, inlang, outlang_list, outfilename):
 # get the input word list
 inputfile = sys.argv[1]
 
+dateregex = re.compile('\d{2}\.\d{2}\.\d{4}')
+# if the file is saved from AutoNotes then it is a string
+# read a string from a file
+if '_AutoNotes' in inputfile:
+    with open(inputfile, 'r', encoding = 'utf-8') as f:
+        first_line = f.readline()
